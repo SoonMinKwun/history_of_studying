323cd3ea043e3664261ca75140555aa19aca5585
SoonMinKwun <52186166+SoonMinKwun@users.noreply.github.com>
2021-08-27 16:17:02 +0900

    파이썬 DB 기초
    파이썬 SQLite 기초 강의
    네이버 부스트코스
    https://www.boostcourse.org/cs202

---
+++ PY4E_DB/hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "6yVstQWziLOa1DEs7ORT9UMWD",
+            "consumer_secret": "Q0dQvanoamCnoZK6SeYfoml7DnK0TUfKwOMiVZJiavfcTnZQ4I",
+            "token_key": "1280099970180210688-PKjzuAlCBUjqudrIq7ItKo77j6Q67d",
+            "token_secret": "gOtFd6upuGjPsK4Gcywpf49J8rx0ExhFmeiejPQv3TLHN"}
