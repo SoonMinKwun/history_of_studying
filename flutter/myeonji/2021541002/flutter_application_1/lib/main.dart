01c52a76d9a095fe032b752be696d78f939a56c5
SoonMinKwun <52186166+SoonMinKwun@users.noreply.github.com>
2021-10-02 00:56:06 +0900

    플러터 과제

--- flutter/myeonji/2021541002/flutter_application_1/lib/main.dart
+++ flutter/myeonji/2021541002/flutter_application_1/lib/main.dart
@@ -41,67 +41,73 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("BMI 계산기"),
-        centerTitle: true,
-      ),
-      resizeToAvoidBottomInset: false,
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(children: [
-          TextField(
-            controller: hController,
-            keyboardType: TextInputType.number,
-            decoration:
-                InputDecoration(border: OutlineInputBorder(), labelText: "키"),
-            onChanged: (value) {
-              setState(() {
-                h = double.parse(value);
-              });
-            },
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          TextField(
-            controller: wController,
-            keyboardType: TextInputType.number,
-            decoration:
-                InputDecoration(border: OutlineInputBorder(), labelText: "몸무게"),
-            onChanged: (value) {
-              setState(() {
-                w = double.parse(value);
-              });
-            },
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          SizedBox(
-            height: 60.0,
-            width: 90.0,
-            child: ElevatedButton(
-              onPressed: () {
-                double _bmi = w / ((h / 100) * (h / 100));
-                setState(() {
-                  bmi = _bmi;
-                });
-              },
-              child: Text("확인"),
+        appBar: AppBar(
+          title: const Text("BMI 계산기"),
+          centerTitle: true,
+        ),
+        resizeToAvoidBottomInset: false,
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(children: [
+            Center(
+              child: Text(
+                "BMI : ${bmi.toStringAsFixed(3)}",
+                style: TextStyle(fontSize: 35.0),
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          Center(
-            child: Text(
-              "BMI : ${bmi.toStringAsFixed(3)}",
-              style: TextStyle(fontSize: 35.0),
+            const SizedBox(
+              height: 20.0,
             ),
-          ),
-        ]),
-      ),
-    );
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Flexible(
+                fit: FlexFit.tight,
+                child: Container(
+                  child: TextField(
+                    controller: hController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), labelText: "키"),
+                    onChanged: (value) {
+                      setState(() {
+                        h = double.parse(value);
+                      });
+                    },
+                  ),
+                ),
+              ),
+              Flexible(
+                fit: FlexFit.tight,
+                child: Container(
+                  child: TextField(
+                    controller: wController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), labelText: "몸무게"),
+                    onChanged: (value) {
+                      setState(() {
+                        w = double.parse(value);
+                      });
+                    },
+                  ),
+                ),
+              )
+            ]),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  double _bmi = w / ((h / 100) * (h / 100));
+                  setState(() {
+                    bmi = _bmi;
+                  });
+                },
+                child: Text("확인"),
+              ),
+            ),
+          ]),
+        ));
   }
 
   @override
