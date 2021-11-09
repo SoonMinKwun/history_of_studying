930499a0e4c373e0d9eac71b2af32bf1057954b9
SoonMinKwun <52186166+SoonMinKwun@users.noreply.github.com>
2021-11-09 17:13:14 +0900

    플러터 프로젝트

--- flutter/meonji/lib/screens/weather_screen.dart
+++ flutter/meonji/lib/screens/weather_screen.dart
@@ -5,6 +5,7 @@ import 'package:google_fonts/google_fonts.dart'; // 폰트
 import 'package:flutter_svg/flutter_svg.dart'; // svg 사용하기 위함
 import 'package:intl/intl.dart'; // DateTime Format
 import 'package:timer_builder/timer_builder.dart'; // 시스템 시간 불러오기 위함
+import 'package:meonji/model/model.dart'; // condition에 따른 svg 표시 조건문
 
 // 빌더를 전달받음
 class WeatherScreen extends StatefulWidget {
@@ -19,6 +20,8 @@ class WeatherScreen extends StatefulWidget {
 class _WeatherScreenState extends State<WeatherScreen> {
   late String cityName; // 도시명
   late int temp; // 온도
+  late Widget icon; // svg (아이콘 모양)
+  late String des; // 날씨 설명
   var date = DateTime.now(); // 오늘 날짜
 
   @override
@@ -30,9 +33,13 @@ class _WeatherScreenState extends State<WeatherScreen> {
 
   // 날씨 업데이트
   void updateData(dynamic weatherData) {
-    double temp2 = weatherData['main']['temp'];
+    Model model = Model(); // 날씨 Condition Model 객체
+    double temp2 = weatherData['main']['temp']; // 온도
+    int condition = weatherData['weather'][0]['id']; // 날씨 id
+    des = weatherData['weather'][0]['description']; // 날씨 설명
     temp = temp2.round(); // 반올림
-    cityName = weatherData['name'];
+    cityName = weatherData['name']; // 도시명
+    icon = model.getWeatherIcon(condition); // 날씨 Condition 불러오기
 
     print(temp);
     print(cityName);
@@ -105,7 +112,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
                                   height: 150.0,
                                 ),
                                 // 위치
-                                Text('Seoul',
+                                Text('$cityName',
                                     style: GoogleFonts.lato(
                                         fontSize: 35.0,
                                         fontWeight: FontWeight.bold,
@@ -145,22 +152,19 @@ class _WeatherScreenState extends State<WeatherScreen> {
                             Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text('18\u2103', // 기온 표시 유니코드
+                                Text('$temp\u2103', // 기온 표시 유니코드
                                     style: GoogleFonts.lato(
                                         fontSize: 85.0,
                                         fontWeight: FontWeight.w300,
                                         color: Colors.white)),
                                 Row(
                                   children: [
-                                    SvgPicture.asset(
-                                      'svg/climacon-sun.svg',
-                                      color: Colors.white,
-                                    ),
+                                    icon,
                                     SizedBox(
                                       width: 10.0,
                                     ),
                                     Text(
-                                      'clear sky',
+                                      '$des',
                                       style: GoogleFonts.lato(
                                           fontSize: 16.0, color: Colors.white),
                                     ),
